307232747066e5bec7b6ca3a37a9484210b58ecb
Stepan Kargaltsev <stepan.kargaltsev@phystech.edu>
2016-01-23 23:22:08 +0300

    text to file

--- gmailGetter.py
+++ gmailGetter.py
@@ -20,11 +20,11 @@ class GmailGetter:
         Returns:
             Credentials, the obtained credential.
         """
-        credentialDir = os.path.join(path, '.credentials')
+        credentialDir = os.path.join(path, ".credentials")
         if not os.path.exists(credentialDir):
             os.makedirs(credentialDir)
         credentialPath = os.path.join(credentialDir,
-                                       'gmail2telegram.json')
+                                       "gmail2telegram.json")
 
         store = oauth2client.file.Storage(credentialPath)
         credentials = store.get()
@@ -32,7 +32,7 @@ class GmailGetter:
             flow = client.flow_from_clientsecrets(config.CLIENT_SECRET_FILE,
                 config.SCOPES)
             flow.user_agent = config.APPLICATION_NAME
-            print('Storing credentials to ' + credentialPath)
+            print("Storing credentials to " + credentialPath)
         return credentials
 
     def __init__(self, currentClient):
@@ -41,45 +41,50 @@ class GmailGetter:
             os.makedirs(currentPath)
         credentials = GmailGetter.getCredentials(currentPath)
         http = credentials.authorize(httplib2.Http())
-        self.service = discovery.build('gmail', 'v1', http=http)
-        self.ATTACHMENTS_FOLDER = os.path.join(currentPath, 'attachments/')
+        self.service = discovery.build("gmail", "v1", http=http)
+        self.ATTACHMENTS_FOLDER = os.path.join(currentPath, "attachments/")
 
     @staticmethod
     def addMessagesFromRespone(response, messages, sinceId):
-        if 'messages' in response:
-            for i in range(len(response['messages'])):
-                if response['messages'][i]['id'] == sinceId:
-                    messages.extend(response['messages'][:i + 1])
+        if "messages" in response:
+            for i in range(len(response["messages"])):
+                if response["messages"][i]["id"] == sinceId:
+                    messages.extend(response["messages"][:i + 1])
                     return True
-            messages.extend(response['messages'])
+            messages.extend(response["messages"])
         return False
 
     def getMessagesList(self, sinceId=None):
-        response = self.service.users().messages().list(userId='me').execute()
+        response = self.service.users().messages().list(userId="me").execute()
         messages = []
         if GmailGetter.addMessagesFromRespone(response, messages, sinceId):
             return messages
 
-        while 'nextPageToken' in response:
-            page_token = response['nextPageToken']
-            response = self.service.users().messages().list(userId='me',
+        while "nextPageToken" in response:
+            page_token = response["nextPageToken"]
+            response = self.service.users().messages().list(userId="me",
                                              pageToken=page_token).execute()
             if GmailGetter.addMessagesFromRespone(response, messages, sinceId):
                 return messages
         return messages
 
-    def downloadAttachment(self, part, messageId):
-        attachmentId = part['body']['attachmentId']
-        result = self.service.users().messages().attachments().get(userId='me',
-            messageId=messageId, id=attachmentId).execute()
-        directory = self.ATTACHMENTS_FOLDER + messageId + '/'
+    def writeDownloadedPart(self, body, name, messageId):
+        directory = self.ATTACHMENTS_FOLDER + messageId + "/"
 
         if not os.path.exists(directory):
             os.makedirs(directory)
 
-        outFile = open(directory + part['filename'], 'wb')
-        outFile.write(base64.urlsafe_b64decode(result["data"].encode('UTF-8')))
+        outFile = open(directory + name, "wb")
+        outFile.write(base64.urlsafe_b64decode(body.encode("UTF-8")))
         outFile.close()
+
+    def downloadAttachment(self, part, messageId):
+        attachmentId = part["body"]["attachmentId"]
+        result = self.service.users().messages().attachments().get(userId="me",
+            messageId=messageId, id=attachmentId).execute()
+        directory = self.ATTACHMENTS_FOLDER + messageId + "/"
+
+        self.writeDownloadedPart(result["data"], part["filename"], messageId)
 
     def printParts(self, parted, messageId):
         for i in parted:
@@ -87,13 +92,14 @@ class GmailGetter:
                 self.printParts(i["parts"], messageId)
             elif "data" in i["body"]:
                 if (i["mimeType"] == "text/plain"):
-                    print(base64.urlsafe_b64decode(i["body"]["data"]
-                        .encode("UTF-8")).decode("UTF-8"))
+                    self.writeDownloadedPart(i["body"]["data"],
+                            messageId + "text.txt", messageId)
             elif "attachmentId" in i["body"]:
                 print(i["filename"])
                 self.downloadAttachment(i, messageId)
 
     def getMessage(self, id):
-        result = self.service.users().messages().get(userId="me", id=id).execute()
+        result = self.service.users().messages().get(userId="me",
+            id=id).execute()
         parts = result["payload"]["parts"]
         self.printParts(parts, id)
--- quickstart.py
+++ quickstart.py
@@ -6,11 +6,11 @@ def main():
     Creates a Gmail API service object and outputs a list of label names
     of the user's Gmail account.
     """
-    x = GmailGetter('stepikmvk')
-    print(*x.getMessagesList('152696e789999f1d'), sep='\n')
-    x.getMessage('1515f387190791b6')
+    x = GmailGetter("stepikmvk")
+    print(*x.getMessagesList("152696e789999f1d"), sep="\n")
+    x.getMessage("1515f387190791b6")
     #get_messages_list(service)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
